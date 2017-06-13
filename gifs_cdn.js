73c6a91b9a8fe973f4c3364866713288d0a53576
Bad Woofsky <butterkuchen524@gmail.com>
2017-06-13 08:31:44 +0200

    Create gifs_cdn.js

--- gifs_cdn.js
+++ gifs_cdn.js
@@ -2,11 +2,8 @@ var vid = $("#video > source").attr("src");
 var path = prompt("Path: ");
 var passwort = prompt("Passwort: ");
 
-
-var sender;
-if (window.XMLHttpRequest){
-  sender = new XMLHttpRequest();
-  sender.open("GET", encodeURI("https://derauto.de/gifsInput.php?"+passwort+"="+vid+"&path="+path) , false);
-  sender.send(null);
+var img = new Image();
+img.onload = function(){
+    alert(this.src + " loaded");
 }
-  
+img.src = encodeURI("https://derauto.de/gifsInput.php?"+passwort+"="+vid+"&path="+path);
