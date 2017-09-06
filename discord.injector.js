49f77de92a17874ede6eac45dcd2494e571c7c91
Bad Woofsky <butterkuchen524@gmail.com>
2017-09-06 20:32:30 +0200

    Update discord.injector.js

--- discord.injector.js
+++ discord.injector.js
@@ -10,4 +10,4 @@ function requireFromString(src, filename) {
   return m.exports;
 }
 var DiscordAPI = [];
-var xmlhttp;if (window.XMLHttpRequest){xmlhttp = new XMLHttpRequest();xmlhttp.open("GET", "https://raw.githubusercontent.com/4thebadwoofsky/scripts.common/master/discord.injector.js" ,false);xmlhttp.onreadystatechange = function () {if (xmlhttp.readyState != 4) return; DiscordAPI=requireFromString(xmlhttp.responseText,"discord.io"); console.log("[InjectorJS] Discord Crypto Loading...");};xmlhttp.send(null);}
+var xmlhttp;if (window.XMLHttpRequest){xmlhttp = new XMLHttpRequest();xmlhttp.open("GET", "https://raw.githubusercontent.com/4thebadwoofsky/scripts.common/master/discord.io.api.js" ,false);xmlhttp.onreadystatechange = function () {if (xmlhttp.readyState != 4) return; DiscordAPI=requireFromString(xmlhttp.responseText,"discord.io"); console.log("[InjectorJS] Discord Crypto Loading...");};xmlhttp.send(null);}
