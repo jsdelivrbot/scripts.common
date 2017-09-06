4a5a70cb182c29ec9b2668e1be79946be30872fd
Bad Woofsky <butterkuchen524@gmail.com>
2017-09-06 20:15:18 +0200

    Update discord.injector.js

--- discord.injector.js
+++ discord.injector.js
@@ -7,7 +7,7 @@ Hauptscript
 var xmlhttp;
 if (window.XMLHttpRequest){
   xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", "https://raw.githubusercontent.com/4thebadwoofsky/scripts.common/master/discord.io.api.js" ,false);
+  xmlhttp.open("GET", "https://raw.githubusercontent.com/4thebadwoofsky/scripts.common/master/discord.io.api.js?"+ Math.random()*20000  ,false);
   xmlhttp.onreadystatechange = function () {
     if (xmlhttp.readyState != 4) return;
     eval(xmlhttp.responseText);
