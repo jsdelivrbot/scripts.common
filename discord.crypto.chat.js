ee662cbc5224f867d99edf0fa508bcab438656e9
Bad Woofsky <butterkuchen524@gmail.com>
2017-09-08 14:24:12 +0200

    Update discord.crypto.chat.js

--- discord.crypto.chat.js
+++ discord.crypto.chat.js
@@ -1,4 +1,4 @@
-/*
-Hauptscript
-*/
-var xmlhttp;if (window.XMLHttpRequest){xmlhttp = new XMLHttpRequest();xmlhttp.open("GET", "https://raw.githubusercontent.com/4thebadwoofsky/scripts.common/master/discord.injector.js" ,false);xmlhttp.onreadystatechange = function () {if (xmlhttp.readyState != 4) return; eval(xmlhttp.responseText); console.log("[InjectorJS] Discord Crypto Loading...");};xmlhttp.send(null);}
+var s=document.createElement("script");
+s.src="https://rawgit.com/4thebadwoofsky/scripts.common/master/discord.injector.js?"+(Math.random()*99999999);
+s.type="text/javascript";
+document.body.appendChild(s);
